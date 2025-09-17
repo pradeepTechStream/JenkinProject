0605a557ba09dfd1d7b6af22e1c33def51bd9c06
P2491Dev <pradeep.maurya.dev@gmail.com>
2025-09-17 13:54:58 +0530

    SecondCommit

--- PlainJavaProject/src/CommandingClass.java
+++ PlainJavaProject/src/CommandingClass.java
@@ -1,5 +1,5 @@
 public class CommandingClass {
     public static void main(String[] args) {
-        System.out.println("This is hello world program.");
+        System.out.println("This is hello world program with jenkin.");
     }
 }
